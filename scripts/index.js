05b92a5850b72d64c82310f955c46d49da34e041
lundk1 <karlotta.lund@aalto.fi>
2024-02-29 19:37:34 +0200

    Nyt sivupalkin "projektit" nappi toimii
    Nappia painamalla voi piilottaa kaiken muun paitsi projekteiksi tägätyn
    sisällön.
    Painamalla uudestaan, myös blogipostaukset tulevat näkyviin.

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,16 @@ document.getElementById("closebtn").addEventListener("click", closeNav);
 
 
 //Elementtien filtteröinti painamalla sivupalkin "projektit" nappia
+const projektitFilter = document.getElementById("projektitFilter");
+const mobiiliFilter = document.getElementById("projektitFilterMobiili");
+const blogit = document.getElementsByClassName("blogiPostaus");
+
+function filterBlogsOut() {
+    for (let i = 0; i < blogit.length; i++) {
+        let kierroksenBlogi = blogit[i];
+        kierroksenBlogi.classList.toggle("hideContent");
+    }
+}
+
+projektitFilter.addEventListener("click", filterBlogsOut);
+mobiiliFilter.addEventListener("click", filterBlogsOut);
