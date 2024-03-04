906797e793c133080052ef9d13a472bfa28f2660
lundk1 <karlotta.lund@aalto.fi>
2024-03-04 14:08:03 +0200

    Nyt myös projektit voi suodattaa ulos Painamalla "Minusta" voi nähdä vain blogit aloitettu myös aukeavan modaalin tekeminen. Tämänhetkinen modaali on tarkoitus avata "Suosittelijat"-painikkeesta.

--- scripts/index.js
+++ scripts/index.js
@@ -16,17 +16,54 @@ document.getElementById("closebtn").addEventListener("click", closeNav);
 
 
 
-//Elementtien filtteröinti painamalla sivupalkin "projektit" nappia
+//Elementtien filtteröinti painamalla sivupalkin "projektit" tai "minusta" nappeja
 const projektitFilter = document.getElementById("projektitFilter");
 const mobiiliFilter = document.getElementById("projektitFilterMobiili");
+const projektitTeksti = document.getElementById("projektitTeksti");
+const projektitTekstiMobiili = document.getElementById("projektitTekstiMobiili");
+
+const minustaFilter = document.getElementById("minustaFilter");
+const mobiiliMinustaFilter = document.getElementById("minustaFilterMobiili");
+const minustaTeksti = document.getElementById("minustaTeksti");
+const minustaTekstiMobiili = document.getElementById("minustaTekstiMobiili");
+
 const blogit = document.getElementsByClassName("blogiPostaus");
+const projektit = document.getElementsByClassName("projektiPostaus");
+
 
 function filterBlogsOut() {
     for (let i = 0; i < blogit.length; i++) {
         let kierroksenBlogi = blogit[i];
         kierroksenBlogi.classList.toggle("hideContent");
     }
+
+    if (blogit[0].classList.contains("hideContent")) {
+        projektitTeksti.innerHTML = "Kaikki sisältö";
+        projektitTekstiMobiili.innerHTML = "Kaikki sisältö";
+    }
+    else {
+        projektitTeksti.innerHTML = "Projektit";
+        projektitTekstiMobiili.innerHTML = "Projektit";
+    }
+}
+
+function filterProjectsOut() {
+    for (let i = 0; i < projektit.length; i++) {
+        let kierroksenProjekti = projektit[i];
+        kierroksenProjekti.classList.toggle("hideContent");
+    }
+
+    if (projektit[0].classList.contains("hideConten")) {
+        minustaTeksti.innerHTML = "Kaikki sisältö";
+        minustaTekstiMobiili.innerHTML = "Kaikki sisältö";
+    }
+    else {
+        minustaTeksti.innerHTML = "Minusta";
+        minustaTekstiMobiili.innerHTML = "Minusta";
+    }
 }
 
 projektitFilter.addEventListener("click", filterBlogsOut);
 mobiiliFilter.addEventListener("click", filterBlogsOut);
+minustaFilter.addEventListener("click", filterProjectsOut);
+mobiiliMinustaFilter.addEventListener("click", filterProjectsOut);
